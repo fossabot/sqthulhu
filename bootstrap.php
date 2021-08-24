f3ede6ec27c76f19aa53804b91b01d5393b8eea0
Alexandros Koutroulis <icyd3mon@gmail.com>
2021-08-24 21:19:16 +0300

    Fix bootstraper in order to properly load local .env files

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,11 @@
 <?php
 
+use Symfony\Component\Dotenv\Dotenv;
+
 require_once 'vendor/autoload.php';
 
-$dotenv = new \Symfony\Component\Dotenv\Dotenv();
-$dotenv->load( __DIR__ . '/.env.test.dist', __DIR__ . '/.env.test');
+$dotenv = new Dotenv();
+$dotenv->load( __DIR__ . '/.env.test.dist');
+if (file_exists(__DIR__ . '/.env.test')) {
+    $dotenv->overload( __DIR__ . '/.env.test');
+}
